0c51e75f698787a1ea503371fef9b3c054b38e57
odilonjk <kroger.odilonj@gmail.com>
2016-01-28 09:16:12 -0200

    Fixed if.rb exercise.

--- if.rb
+++ if.rb
@@ -1,5 +1,6 @@
 print "Hi! Please enter a code with 4 digits: "
-digits = Integer(gets.chomp.length)
+input = Integer(gets.chomp)
+digits = input.to_s.length
 
 if digits > 4
     puts "You've entered a code with more than 4 digits. Operation cancealed."
