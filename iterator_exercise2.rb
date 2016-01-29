53b082cb080fa112cfa987e43c9582d9441276bb
odilonjk <kroger.odilonj@gmail.com>
2016-01-29 12:28:32 -0200

    Add second iterator exercise.

---
+++ iterator_exercise2.rb
@@ -0,0 +1,10 @@
+lunch_order = {
+  "Ryan" => "wonton soup",
+  "Eric" => "hamburger",
+  "Jimmy" => "sandwich",
+  "Sasha" => "salad",
+  "Cole" => "taco"
+}
+lunch_order.each do |client,lunch|
+    puts lunch
+end
